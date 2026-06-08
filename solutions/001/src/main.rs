e9e51bd42f45cd4ecdddc3c89d7a240023ef8dd8
kacper <kacper.kielbasa5000@gmail.com>
2026-06-08 16:07:01 +0200

    dodanie rozwiązania do 001

--- solutions/001/src/main.rs
+++ solutions/001/src/main.rs
@@ -1,12 +1,20 @@
 fn solve(limit: u32) -> u32 {
-    let mut sum = 0;
-    for x in 1..limit {
-        if x % 3 == 0 || x % 5 == 0 {
-            sum += x;
-        }
+    fn sum_multiples(k: u32, limit: u32) -> u32 {
+        let n = (limit - 1) / k;
+        k * n * (n + 1) / 2
     }
-    sum
+    sum_multiples(3, limit) + sum_multiples(5, limit) - sum_multiples(15, limit)
 }
+
+// fn solve(limit: u32) -> u32 {
+//     let mut sum = 0;
+//     for x in 1..limit {
+//         if x % 3 == 0 || x % 5 == 0 {
+//             sum += x;
+//         }
+//     }
+//     sum
+// }
 
 // fn solve(limit: u32) -> u32 {
 //     (1..limit).filter(|x| x % 3 == 0 || x % 5 == 0).sum()
