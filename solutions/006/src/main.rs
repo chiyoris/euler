4d0548e245103beaa7916706aa8a50aad3d3a028
kacper <kacper.kielbasa5000@gmail.com>
2026-06-10 21:53:34 +0200

    skończenie zadania 006

---
+++ solutions/006/src/main.rs
@@ -0,0 +1,31 @@
+// https://projecteuler.net/problem=006
+
+// cargo run -p euler_006 --release
+
+// use euler_utils::*;
+fn solve(limit: u32) -> u32 {
+    let mut sum_of_squres: u32 = 0;
+    let mut square_of_sums: u32 = 0;
+
+    for x in 1..=limit {
+        sum_of_squres += x.pow(2);
+        square_of_sums += x;
+    }
+
+    square_of_sums.pow(2) - sum_of_squres
+}
+
+fn main() {
+    let answer = solve(100);
+    println!("rozwiązanie dla zadania 006: {}", answer);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        assert_eq!(solve(10), 2640);
+    }
+}
