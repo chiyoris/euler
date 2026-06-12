bb5e67f39b303be6a553842b613eb07214ef1eaa
kacper <kacper.kielbasa5000@gmail.com>
2026-06-12 15:15:03 +0200

    skończenie zadania 007

---
+++ solutions/007/src/main.rs
@@ -0,0 +1,27 @@
+// https://projecteuler.net/problem=007
+
+// cargo run -p euler_007 --release
+
+// use euler_utils::*;
+use primes::{PrimeSet, Sieve};
+
+fn solve(n: u64) -> u64 {
+    let mut pset = Sieve::new();
+
+    pset.iter().nth(n as usize - 1).unwrap()
+}
+
+fn main() {
+    let answer = solve(10_001);
+    println!("rozwiązanie dla zadania 007: {}", answer);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // #[test]
+    // fn test_example() {
+    //     assert_eq!(solve(), 0);
+    // }
+}
