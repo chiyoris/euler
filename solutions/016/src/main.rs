82fc92fd44640cb579e95aa8ae0bd2e2017e1770
chiyoris <kacper.kielbasa5000@gmail.com>
2026-07-03 00:40:51 +0200

    rozpoczęcie zadania 016

---
+++ solutions/016/src/main.rs
@@ -0,0 +1,26 @@
+// https://projecteuler.net/problem=016
+
+// cargo run -p euler_016 --release  
+
+// use euler_utils::*;
+
+
+
+fn solve() -> u64 {
+    0
+}
+
+fn main() {
+    let answer = solve();
+    println!("rozwiązanie dla zadania 016: {}", answer);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        assert_eq!(solve(15), 26);
+    }
+}
